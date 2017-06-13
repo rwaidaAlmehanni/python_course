070f39b07ec17f337f0c73e6e6dbd01148c6beb3
rwaida <rwaidaAlmehanni>
2017-06-13 11:38:53 +0300

    the same chapter/problem20/

--- get_started/files/problem20.py
+++ get_started/files/problem20.py
@@ -1,1 +1,9 @@
 #Implement unix command grep. The grep command takes a string and a file as arguments and prints all lines in the file which contain the specified string.
+import sys
+def grepfun():
+	x=open(sys.argv[1]).readlines()
+	for i in x:
+		if sys.argv[2] in i:
+			print i
+
+grepfun()
