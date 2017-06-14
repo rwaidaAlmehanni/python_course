b2fc51b0fd6c69166bca8642e2c9b175bb88a1e7
rwaida <rwaidaAlmehanni>
2017-06-14 13:25:24 +0300

    dictionary/problem38/Write a function invertdict to interchange keys and values in a dictionary. For simplicity, assume that all values are unique.

--- get_started/dictionary/problem38.py
+++ get_started/dictionary/problem38.py
@@ -1,1 +1,8 @@
 #Write a function invertdict to interchange keys and values in a dictionary. For simplicity, assume that all values are unique.
+def invertdict(f):
+    fr={}
+    for i in f:
+      fr[f[i]]=i
+    print fr
+
+invertdict({'x': 1, 'y': 2, 'z': 3})
