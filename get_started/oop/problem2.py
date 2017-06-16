37f974290aa7aa43823f52e4c202f78474c29619
rwaida <rwaidaAlmehanni>
2017-06-16 16:26:31 +0300

    problem2/what is the output?

---
+++ get_started/oop/problem2.py
@@ -0,0 +1,15 @@
+#Problem 2: What will be the output of the following program?
+
+try:
+    print "a"
+except:
+    print "b"
+else:
+    print "c"
+finally:
+    print "d"
+
+# a
+# c
+# d
+#coz it will execute try or except not together
