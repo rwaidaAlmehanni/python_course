90e95d1123599c1e2231cfbd48a6b904a02825d9
rwaida <rwaidaAlmehanni>
2017-06-16 15:51:14 +0300

    oop/problem1/What will the output of the following program./python problem1.py

---
+++ get_started/oop/problem1.py
@@ -0,0 +1,16 @@
+#What will the output of the following program.
+class A:
+    def f(self):
+        return self.g()
+
+    def g(self):
+        return 'A'
+
+class B(A):
+    def g(self):
+        return 'B'
+
+a = A()
+b = B()
+print a.f(), b.f()# A B
+print a.g(), b.g()#A B
