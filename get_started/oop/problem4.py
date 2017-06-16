a469553f92632c247ee4ad0df949ba0e3cd26ec0
rwaida <rwaidaAlmehanni>
2017-06-16 16:33:46 +0300

    problem4.py/what is the output

---
+++ get_started/oop/problem4.py
@@ -0,0 +1,17 @@
+#Problem 4: What will be the output of the following program?
+
+def f():
+    try:
+        print "a"
+        return
+    except:
+        print "b"
+    else:
+        print "c"
+    finally:
+        print "d"
+
+f()
+#a
+#d
+
