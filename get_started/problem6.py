5d7d65890158eaac03bf2f5a874fd5dc8ed134d6
rwaida <rwaidaAlmehanni>
2017-06-17 13:12:43 +0300

    try it now

--- get_started/problem6.py
+++ get_started/problem6.py
@@ -1,23 +1,21 @@
 #Write a function to compute the total number of lines of code, ignoring empty and comment lines, in all python files in the specified directory recursively
 
 import sys
+from os import listdir
 def readFileLines(xfile):
-  f={}
   counter=0
   for i in open(xfile).readlines():
     if i!="\n" and i[0]!="#":
       counter+=1
-  f[str(xfile)]=counter
-
-  print f             
+  return counter             
 
 
 def readDirFiles():
-  f=[]
+  f={}
   x=listdir(sys.argv[1])
   for i in x:
   	if str(i).index(".")!=0:
-      f.append(readFileLines(i))		 
+      f[str(i)]=readFileLines(i)		 
   print f
 
 readDirFiles()
